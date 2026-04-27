b44d9162715f318d5080f01c73ed34d420ed5bfd
mciahel545 <michael.valand@gmail.com>
2026-04-27 13:10:34 +0200

    Refactor: Remove magni_description include, restrict to gen5 only

--- ubiquity_motor_ros2/launch/ubiquity_motor_ros2.launch.py
+++ ubiquity_motor_ros2/launch/ubiquity_motor_ros2.launch.py
@@ -18,7 +18,6 @@ def try_run_motor_node(context, *args, **kwargs):
         # Step 1: Run the stty command to configure the serial port
         to_launch.append(ExecuteProcess(
             cmd=['sudo', 'stty', '-F', '/dev/robot_serial', 'sane'],
-            # cmd=['sudo', 'stty', '-F', '/dev/ttyAMA0', 'sane'],
             shell=True
         ))
 
@@ -34,7 +33,6 @@ def try_run_motor_node(context, *args, **kwargs):
             output='screen',
             parameters=[config_file],
             remappings=[
-                # ('/ubiquity_velocity_controller/cmd_vel_unstamped', '/cmd_vel'),  # Remap the cmd_vel topic
                 ('/ubiquity_velocity_controller/odom', '/odom')         # Remap odom
             ]
         ))
@@ -60,54 +58,10 @@ def try_run_motor_node(context, *args, **kwargs):
 
 
 def generate_launch_description():
-
-    description_share = os.path.join(get_package_share_directory('magni_description'))
-
     # Declare the launch arguments
-    camera_arg = DeclareLaunchArgument('camera_extrinsics_file', default_value=os.path.join(description_share, 'urdf/extrinsics/camera_extrinsics_forward.yaml'))
-    lidar_arg = DeclareLaunchArgument('lidar_extrinsics_file', default_value=os.path.join(description_share, 'urdf/extrinsics/lidar_extrinsics_top_plate_center.yaml'))
-    sonars_arg = DeclareLaunchArgument('sonars_installed', default_value='false')
-    shell_arg = DeclareLaunchArgument('shell_installed', default_value='false')
-    tower_arg = DeclareLaunchArgument('tower_installed', default_value='false')
-    generation_arg = DeclareLaunchArgument('generation')
-
-    # Use LaunchConfiguration to get the values
-    camera_file = LaunchConfiguration('camera_extrinsics_file')
-    lidar_file = LaunchConfiguration('lidar_extrinsics_file')
-    sonars = LaunchConfiguration('sonars_installed')
-    shell = LaunchConfiguration('shell_installed')
-    tower = LaunchConfiguration('tower_installed')
-
-    # Path to the magni_description launch file
-    magni_description_launch = os.path.join(
-        get_package_share_directory('magni_description'),
-        'launch',
-        'magni_description.launch.py'
-    )
-
-    # Include the magni_description launch file
-    magni_description_include = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(magni_description_launch),
-        launch_arguments={
-            'camera_extrinsics_file': camera_file,
-            'lidar_extrinsics_file': lidar_file,
-            'sonars_installed': sonars,
-            'shell_installed': shell,
-            'tower_installed': tower,
-        }.items()
-    )
+    generation_arg = DeclareLaunchArgument('generation', default_value='gen5')
 
     return launch.LaunchDescription([
-        # run_xacro,  # Runs the xacro process
-        # robot_state_publisher,  # Starts the robot_state_publisher with xacro output
-        camera_arg,
-        lidar_arg,
-        sonars_arg,
-        shell_arg,
-        tower_arg,
-        magni_description_include,
+        generation_arg,
         OpaqueFunction(function=try_run_motor_node)
-        # serial_config,
-        # controller_node,
-        # spawn_controller
     ])
